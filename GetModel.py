37d02807dac8dcd919004dd3e1c99941e4eee728
Robert Juric <robert.juric@gmail.com>
2018-11-16 16:27:01 -0600

    Update code to add column to an updated inventory file

--- GetModel.py
+++ GetModel.py
@@ -18,11 +18,21 @@ with open ('Inventory.csv', 'r') as inf, open('UpdatedInventory.csv', 'w') as ou
 	for row in reader:
 		this_ip = row['IP Address']
 		print('Connecting to:', this_ip)
-		net_connect = ConnectHandler(device_type='cisco_ios', ip=this_ip, username='admin', password='nowayjose')
-		model_output = net_connect.send_command("show version | i Model number")
-		model_detail = re.split('[:]', model_output)
-		model_info = model_detail[1].lstrip(' ')
-		print(model_info)
+		net_connect = ConnectHandler(device_type='cisco_ios', ip=this_ip, username='admin', password='pw')
+		#Works but returns empty on IOS-XE
+		#model_output = net_connect.send_command("show version | i Model number")
+		#Works better across multiple platforms, but is still missing:
+		#show version | i Model Number appears to work on IOS-XE
+		#"show version | i bytes of" to get similar out, some devices say 'bytes of physical memory'
+		model_output = net_connect.send_command("show version | i bytes of memory")
+		model_detail = re.split(' ', model_output)
+		if model_output:
+			print(model_detail[1])
+			model_info = model_detail[1]
+			#strip leading space when splitting "show version | i Model number"
+			#model_info = model_detail[1].lstrip(' ')
+		else:
+			model_info = "empty"
 		print("Writing row")
 		writer.writerow(dict(row,Model=model_info))
 
